53ec912eb5396ccda44488c47b877ce2b1cf8d3b
xeonicca <xeonicca@goods10.(none)>
2014-08-06 18:25:41 +0800

    [bugfix] fixed the decoding function where it may fail when having two trailing paddings

--- base64.js
+++ base64.js
@@ -29,26 +29,51 @@
 
   RT.mybid.base64Decode = function( encoded ){
     encoded = encoded.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-    var i = 0, plain = '';
-    while( i < encoded.length ){
-      var enc1 = base64list.indexOf(encoded[i++]);
-      var enc2 = base64list.indexOf(encoded[i++]);
-      var enc3 = base64list.indexOf(encoded[i++]);
-      var enc4 = base64list.indexOf(encoded[i++]);
+    var i = 0, plain = '', last_padding = false, second_last_padding = false, imax = encoded.length, 
+    enc1, enc2, enc3, enc4, dec1, dec2, dec3, b64str;
 
-      var dec1 = String.fromCharCode(enc1<<2 | ((enc2&48)>>4));
+    if( '=' === encoded.charAt(imax-1) ){
+      last_padding = true;
+      if( '=' === encoded.charAt(imax-2) ){
+        second_last_padding = true;
+      }
+      imax -=4;
+    }
+
+    while( i < imax ){
+      enc1 = base64list.indexOf(encoded[i++]);
+      enc2 = base64list.indexOf(encoded[i++]);
+      enc3 = base64list.indexOf(encoded[i++]);
+      enc4 = base64list.indexOf(encoded[i++]);
+
+      dec1 = String.fromCharCode(enc1<<2 | ((enc2&48)>>4));
       plain += dec1;
       
-      var dec2 = (enc2&15)<<4 | ((enc3&60)>>2);
+      dec2 = (enc2&15)<<4 | ((enc3&60)>>2);
       if( dec2 != 64 )
         plain +=  String.fromCharCode(dec2);
       
-      var dec3 = (enc3&3)<<6 | (enc4);
+      dec3 = (enc3&3)<<6 | (enc4);
       if( dec3 != 64 )
         plain +=  String.fromCharCode(dec3);
     }
-    
+
+    if( second_last_padding || last_padding ){
+      enc1 = base64list.indexOf(encoded[i++]);
+      enc2 = base64list.indexOf(encoded[i++]);
+      enc3 = base64list.indexOf(encoded[i++]);
+
+      if( second_last_padding ){
+        b64str = enc1 << 18 | enc2 << 12;
+        plain += String.fromCharCode( b64str >> 16 );
+      }else if( last_padding ){
+        b64str = enc1 << 18 | enc2 << 12 | enc3 << 6;
+        plain += String.fromCharCode( b64str >> 16, (b64str>>8)&255 );
+      }
+    }
+
     return plain;
   };
+
 
 })(RT);
