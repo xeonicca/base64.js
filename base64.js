746dff3cd2ea23b41a8263b7c75de42b1c9d9eb9
xeonicca <xeonicca@goods10.(none)>
2014-08-07 11:21:42 +0800

    refactored the code abit, and added some more test case.

--- base64.js
+++ base64.js
@@ -1,18 +1,23 @@
-(function(namespace){
+/**
+ * The base64 encoding and decoding function.
+ * Currently this only supports Latin1-based text, meaning foreign characters like Chinese 
+ * are not supported.
+ */
+(function( namespace ) {
   namespace.mybid = namespace.mybid || {};
   var base64list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
   RT.mybid.base64Encode = function( plain ){
-    var i = 0, encoded = '';
+    var i = 0, encoded = '', enc1, enc2, enc3, plain1, plain2, plain3;
     while( i < plain.length ){
-      var plain1 = plain.charCodeAt(i++);
-      var plain2 = plain.charCodeAt(i++);
-      var plain3 = plain.charCodeAt(i++);
+      plain1 = plain.charCodeAt(i++);
+      plain2 = plain.charCodeAt(i++);
+      plain3 = plain.charCodeAt(i++);
       
-      var enc1 = plain1 >> 2;
-      var enc2 = (plain1 & 3) << 4 | plain2 >> 4 ;
-      var enc3 = (plain2 & 15) << 2 | plain3 >> 6;
-      var enc4 = plain3 & 63;
+      enc1 = plain1 >> 2;
+      enc2 = (plain1 & 3) << 4 | plain2 >> 4 ;
+      enc3 = (plain2 & 15) << 2 | plain3 >> 6;
+      enc4 = plain3 & 63;
       
       if( isNaN(plain2) ) {
         enc3 = enc4 = 64;
@@ -22,15 +27,14 @@
       }
       encoded += base64list[enc1]+base64list[enc2]+base64list[enc3]+base64list[enc4];
     }
-
     return encoded;
   };
 
 
   RT.mybid.base64Decode = function( encoded ){
     encoded = encoded.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-    var i = 0, plain = '', last_padding = false, second_last_padding = false, imax = encoded.length, 
-    enc1, enc2, enc3, enc4, dec1, dec2, dec3, b64str;
+    var i = 0, plain = '', last_padding = false, second_last_padding = false, imax = encoded.length,
+    enc1, enc2, enc3, enc4, b64str = '';
 
     if( '=' === encoded.charAt(imax-1) ){
       last_padding = true;
@@ -46,16 +50,7 @@
       enc3 = base64list.indexOf(encoded[i++]);
       enc4 = base64list.indexOf(encoded[i++]);
 
-      dec1 = String.fromCharCode(enc1<<2 | ((enc2&48)>>4));
-      plain += dec1;
-      
-      dec2 = (enc2&15)<<4 | ((enc3&60)>>2);
-      if( dec2 != 64 )
-        plain +=  String.fromCharCode(dec2);
-      
-      dec3 = (enc3&3)<<6 | (enc4);
-      if( dec3 != 64 )
-        plain +=  String.fromCharCode(dec3);
+      plain += String.fromCharCode( enc1<<2 | ((enc2&48)>>4), (enc2&15)<<4 | ((enc3&60)>>2), (enc3&3)<<6 | (enc4) );
     }
 
     if( second_last_padding || last_padding ){
@@ -71,9 +66,6 @@
         plain += String.fromCharCode( b64str >> 16, (b64str>>8)&255 );
       }
     }
-
     return plain;
   };
-
-
 })(RT);
